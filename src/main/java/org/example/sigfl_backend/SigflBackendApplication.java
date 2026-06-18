977213edb776cbf54a0133ce90efd346b2c40c3b
Léandre ELISHA <leandreelisha20@gmail.com>
2026-06-18 18:07:02 +0100

    first commit

---
+++ src/main/java/org/example/sigfl_backend/SigflBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.sigfl_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigflBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SigflBackendApplication.class, args);
+    }
+
+}
